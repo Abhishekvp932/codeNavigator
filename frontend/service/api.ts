6f9a4d4d6f36a38f3cd318ad56b4fb938f7e337c
Abhishek vp <avp7580@gmail.com>
2026-04-03 16:12:51 +0530

    (feat : changed env file name and production url)

--- frontend/service/api.ts
+++ frontend/service/api.ts
@@ -1,16 +1,12 @@
 import axios from 'axios';
 
+export const api = axios.create({
+  baseURL:process.env.NEXT_PUBLIC_API_URL,
+    withCredentials:true,
+});
 
-// const isLocal = window.location.hostname === "localhost";
 
 // export const api = axios.create({
-//   baseURL: isLocal
-//     ? "http://localhost:4000"
-//     : "https://codenavigator.ddns.net",
-// });
-
-
-export const api = axios.create({
-    baseURL: "http://localhost:4000",
-    withCredentials:true,
-})
+//     baseURL: "http://localhost:4000",
+//     withCredentials:true,
+// })
